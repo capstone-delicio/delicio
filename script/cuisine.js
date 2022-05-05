6b520496fb91e13872394799694448d719adf688
Renee Du <doozyu@gmail.com>
2022-05-05 16:45:57 -0400

    fix cuisine

--- script/cuisine.js
+++ script/cuisine.js
@@ -18,47 +18,47 @@ const cuisine = [
   },
   {
     restaurant_id: 20,
-    cuisine_type: "",
+    cuisine_type: "American",
   },
   {
     restaurant_id: 21,
-    cuisine_type: "",
+    cuisine_type: "American",
   },
   {
     restaurant_id: 22,
-    cuisine_type: "",
+    cuisine_type: "Middle Eastern",
   },
   {
     restaurant_id: 23,
-    cuisine_type: "",
+    cuisine_type: "Mediterranean",
   },
   {
     restaurant_id: 24,
-    cuisine_type: "",
+    cuisine_type: "American",
   },
   {
     restaurant_id: 25,
-    cuisine_type: "",
+    cuisine_type: "Italian",
   },
   {
     restaurant_id: 26,
-    cuisine_type: "",
+    cuisine_type: "American",
   },
   {
     restaurant_id: 27,
-    cuisine_type: "",
+    cuisine_type: "Japanese",
   },
   {
     restaurant_id: 28,
-    cuisine_type: "",
+    cuisine_type: "American",
   },
   {
     restaurant_id: 29,
-    cuisine_type: "",
+    cuisine_type: "Italian",
   },
   {
     restaurant_id: 30,
     cuisine_type: "",
   },
 ]
-export default cuisine
+module.export = cuisine
