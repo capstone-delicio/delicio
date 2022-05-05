f013ddc680540c5b4e3bd01aca2fcb9d66ba7b5b
yajessicachen <yajessicachen@gmail.com>
2022-05-05 14:40:03 -0400

    starting rest seed

---
+++ script/restaurant.js
@@ -0,0 +1,63 @@
+const restaurant = [
+  {
+    name: 'STK Steakhouse',
+    street: '1114 6th Ave',
+    city:'New York',
+    state: 'New York',
+    zip:10036,
+    phone_number:6466242455
+    email:
+    password:
+    open_time:
+    close_time:
+    description:
+    price_range:
+    website_Url:
+    opentableUrl:
+    profile_picUrl:
+  },
+  {
+    name: 'STK Steakhouse',
+    street:
+    city:
+    state:
+    zip:
+    phone_number:
+    email:
+    password:
+    open_time:
+    close_time:
+    description:
+    price_range:
+    website_Url:
+    opentableUrl:
+    profile_picUrl:
+  },
+  {
+    name: 'STK Steakhouse',
+    street:
+    city:
+    state:
+    zip:
+    phone_number:
+    email:
+    password:
+    open_time:
+    close_time:
+    description:
+    price_range:
+    website_Url:
+    opentableUrl:
+    profile_picUrl:
+  },
+]
+module.export = restaurant
+
+// {
+//   userId: 1,
+//   orderId: 1,
+//   productId: 11,
+//   quantity: 3,
+//   uniPrice: 299,
+//   totalPrice: 897,
+// },
