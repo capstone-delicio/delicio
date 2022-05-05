41848978d643f1fb18d66c71af4c32fcd893b706
Renee Du <doozyu@gmail.com>
2022-05-05 16:37:03 -0400

    finish miami

---
+++ script/cuisine.js
@@ -0,0 +1,64 @@
+const cuisine = [
+  {
+    restaurant_id: 16,
+    cuisine_type: "Japanese",
+  },
+  {
+    restaurant_id: 17,
+    cuisine_type: "Italian",
+
+  },
+  {
+    restaurant_id: 18,
+    cuisine_type: "Amrican",
+  },
+  {
+    restaurant_id: 19,
+    cuisine_type: "Japanese",
+  },
+  {
+    restaurant_id: 20,
+    cuisine_type: "",
+  },
+  {
+    restaurant_id: 21,
+    cuisine_type: "",
+  },
+  {
+    restaurant_id: 22,
+    cuisine_type: "",
+  },
+  {
+    restaurant_id: 23,
+    cuisine_type: "",
+  },
+  {
+    restaurant_id: 24,
+    cuisine_type: "",
+  },
+  {
+    restaurant_id: 25,
+    cuisine_type: "",
+  },
+  {
+    restaurant_id: 26,
+    cuisine_type: "",
+  },
+  {
+    restaurant_id: 27,
+    cuisine_type: "",
+  },
+  {
+    restaurant_id: 28,
+    cuisine_type: "",
+  },
+  {
+    restaurant_id: 29,
+    cuisine_type: "",
+  },
+  {
+    restaurant_id: 30,
+    cuisine_type: "",
+  },
+]
+export default cuisine
