bde96af802dba8ae2f362fb4d6457914a5b9b817
Vida Feng <vidaffeng@gmail.com>
2022-05-06 14:53:30 -0400

    events and bookmarked only

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,13 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
 
-const User = require('./models/User')
-const Restaurant = require('./models/Restaurant')
-const Event = require('./models/Event')
-const Cuisine = require('./models/Cuisine')
-const Restaurant_pics = require('./models/Restaurant_pics')
-const Event_picks = require('./models/Event_picks')
+const User = require("./models/User");
+const Restaurant = require("./models/Restaurant");
+const Event = require("./models/Event");
+const Cuisine = require("./models/Cuisine");
+const Restaurant_pics = require("./models/Restaurant_pics");
+const Event_picks = require("./models/Event_picks");
 
 //associations could go here!
 // friends is the through table
@@ -16,14 +16,17 @@ const Event_picks = require('./models/Event_picks')
 //   foreignKey: 'id',
 // })
 
-// User.belongsToMany(Event, { through: 'event_attendees' })
-// Event.belongsToMany(User, { through: 'event_attendees' })
+User.belongsToMany(Event, { through: "event_attendees" });
+Event.belongsToMany(User, { through: "event_attendees" });
 
-// Restaurant.hasMany(Restaurant_pics, { foreignKey: 'id' })
-// Restaurant_pics.belongsTo(Restaurant)
+Restaurant.hasMany(Restaurant_pics);
+Restaurant_pics.belongsTo(Restaurant);
 
 // Event.hasMany(Event_picks);
 // User.hasMany(Event_picks);
+
+Restaurant.hasMany(Event);
+Event.belongsTo(Restaurant);
 
 // User.belongsToMany(Restaurant, { through: 'bookmarked_restaurants' })
 // Restaurant.belongsToMany(User, { through: 'bookmarked_restaurants' })
@@ -38,4 +41,4 @@ module.exports = {
     Restaurant_pics,
     Event_picks,
   },
-}
+};
