c1544410f697a1ca1395eb240dec36aa9edc09c3
Rachel Or <rachelor7@gmail.com>
2022-05-06 11:44:02 -0500

    friends attendees eventPicks seeds

---
+++ script/attendees.js
@@ -0,0 +1,32 @@
+const attendees = [
+  {
+    eventId: 1,
+    userId: 1,
+  },
+  {
+    eventId: 1,
+    userId: 3,
+  },
+  {
+    eventId: 1,
+    userId: 4,
+  },
+  {
+    eventId: 2,
+    userId: 6,
+  },
+  {
+    eventId: 2,
+    userId: 5,
+  },
+  {
+    eventId: 2,
+    userId: 8,
+  },
+  {
+    eventId: 2,
+    userId: 7,
+  },
+];
+
+module.exports = friends;
---
+++ script/friends.js
@@ -0,0 +1,28 @@
+const friends = [
+  { userId: 1, friendId: 2 },
+  { userId: 2, friendId: 1 },
+  { userId: 1, friendId: 3 },
+  { userId: 3, friendId: 1 },
+  { userId: 1, friendId: 4 },
+  { userId: 4, friendId: 1 },
+  { userId: 1, friendId: 5 },
+  { userId: 5, friendId: 5 },
+  { userId: 1, friendId: 6 },
+  { userId: 6, friendId: 1 },
+  { userId: 6, friendId: 5 },
+  { userId: 5, friendId: 6 },
+  { userId: 6, friendId: 7 },
+  { userId: 7, friendId: 6 },
+  { userId: 6, friendId: 8 },
+  { userId: 8, friendId: 6 },
+  { userId: 9, friendId: 8 },
+  { userId: 8, friendId: 9 },
+  { userId: 5, friendId: 4 },
+  { userId: 4, friendId: 5 },
+  { userId: 5, friendId: 1 },
+  { userId: 1, friendId: 5 },
+  { userId: 7, friendId: 9 },
+  { userId: 9, friendId: 7 },
+];
+
+module.exports = friends;
