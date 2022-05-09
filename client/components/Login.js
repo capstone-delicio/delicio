1e7b2d3976a72d616626c9408aefad5c50d8e28b
yajessicachen <yajessicachen@gmail.com>
2022-05-09 00:50:41 -0400

    added style for login

--- client/components/Login.js
+++ client/components/Login.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { authenticate } from '../store'
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+
+import Button from '@material-ui/core/Button'
 
 const Login = () => {
   const dispatch = useDispatch()
@@ -21,12 +25,28 @@ const Login = () => {
   return (
     <div id="logins">
       <form id="loginForm" onSubmit={handleSubmit} name="login">
-        <div>
+        <Grid
+          container
+          alignItems="center"
+          justifyContent="center"
+          direction="column"
+        >
+          <Grid item>
+            <TextField name="email" label="Email" type="text" />
+          </Grid>
+          <Grid item>
+            <TextField name="password" label="Password" type="password" />
+          </Grid>
+          <Button variant="contained" color="primary" type="submit">
+            Login
+          </Button>
+
+          {/*<Grid container justifyContent="center">
           <label htmlFor="email">
-            <small>Email</small>
+            <small>Email: </small>
           </label>
           <input name="email" type="text" />
-        </div>
+        </Grid>
         <div>
           <label htmlFor="password">
             <small>Password</small>
@@ -35,13 +55,15 @@ const Login = () => {
         </div>
         <div>
           <button type="submit">Login</button>
-        </div>
-        <p>
-          Dont have an account? <Link to="/signup">Sign Up</Link>
-        </p>
-        {error && error.response && (
-          <div style={{ color: 'red' }}> {error.response.data} </div>
-        )}
+        </div>*/}
+
+          <p>
+            Dont have an account? <Link to="/signup">Sign Up</Link>
+          </p>
+          {error && error.response && (
+            <div style={{ color: 'red' }}> {error.response.data} </div>
+          )}
+        </Grid>
       </form>
     </div>
   )
