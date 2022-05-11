d3710ead51e02cd01b44563bc77eb70d74edca57
Vida Feng <vidaffeng@gmail.com>
2022-05-11 19:17:19 -0400

    getRests Thunk connected to questions component

--- client/store/yelp.js
+++ client/store/yelp.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+// const dotenv = require("dotenv");
+// dotenv.config();
 
 // Action Types
 const GET_RESTS = "GET_RESTS";
@@ -15,26 +17,38 @@ const getSingleRest = (id) => ({
   rest: id,
 });
 
+const proxyUrl = `https://cors-anywhere.herokuapp.com/`;
+const auth = {
+  Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
+};
+
 // Thunks
 export const _getRests = (params) => async (dispatch) => {
   // expect params to be an object
   const { location, limit, price, cuisine } = params;
 
-  const auth = {
-    Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
-  };
+  console.log("from yelp.js", location, limit, price, cuisine);
+
   const autocompleteParams = {
     text: cuisine,
     latitude: "",
     longitude: "",
     locale: "en_US",
   };
+
+  console.log("fromthunks", process.env.REACT_APP_YELP_API_KEY);
+
+  console.log("autocompleteParams", autocompleteParams);
+
+  // console.log("dotenv", process.env);
+
   // run cuisine thru autocomplete Yelp API
-  const auto_url = "https://api.yelp.com/v3/autocomplete";
+  const auto_url = `https://api.yelp.com/v3/autocomplete`;
 
   let catArr = [];
+
   try {
-    const { data } = await axios.get(auto_url, {
+    const { data } = await axios.get(proxyUrl + auto_url, {
       headers: auth,
       params: autocompleteParams,
     });
@@ -44,6 +58,9 @@ export const _getRests = (params) => async (dispatch) => {
   }
 
   // take the results of category array and put into Yelp business search api
+
+  // console.log(carArr);
+
   const categories = catArr
     .map((cat) => {
       return cat.alias;
@@ -59,9 +76,9 @@ export const _getRests = (params) => async (dispatch) => {
     price,
   };
 
-  const bizSearch_url = "https://api.yelp.com/v3/businesses/search";
+  const bizSearch_url = `https://api.yelp.com/v3/businesses/search`;
   try {
-    const { data } = await axios.get(bizSearch_url, {
+    const { data } = await axios.get(proxyUrl + bizSearch_url, {
       headers: auth,
       params: busSearchParams,
     });
@@ -75,7 +92,9 @@ export const _getRests = (params) => async (dispatch) => {
 export const _getSingleRest = (id) => async (dispatch) => {
   const bizDetail_url = `https://api.yelp.com/v3/businesses/${id}`;
   try {
-    const { data } = await axios.get(bizDetail_url, { headers: auth });
+    const { data } = await axios.get(proxyUrl + bizDetail_url, {
+      headers: auth,
+    });
     dispatch(getSingleRest(data));
   } catch (err) {
     console.error(err);
@@ -116,4 +135,5 @@ export default function yelp(state = yelpState, action) {
     case GET_SINGLE_REST:
       return { ...state, rest: action.rest };
   }
+  return state;
 }
