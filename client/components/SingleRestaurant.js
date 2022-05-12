60f4710c6e68efc490ae15cd1efed263b7dfc926
Renee Du <doozyu@gmail.com>
2022-05-12 16:54:21 -0400

    single rest

---
+++ client/components/SingleRestaurant.js
@@ -0,0 +1,45 @@
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import React, { useState, useEffect } from "react";
+import { getRests, getSingleRest } from "../store/yelp";
+import axios from 'axios'
+import { useHistory, useParams } from 'react-router-dom'
+import {useSelector,useDispatch} from 'react-redux'
+
+const SingleRestaurant = () => {
+  const { id } = useParams()
+  let history = useHistory()
+  const restraunt = useSelector(state=>state.rest)
+  const dispatch = useDispatch()
+
+  const [formState, setFormState] = useState({
+      id:restraunt.id,
+  })
+
+  const SubmitHandler = async (e) => {
+      e.preventDefault()
+      dispatch(_getSingleRest(formState))
+      history.push('/')
+
+  }
+
+    return (
+      <div className="single-product-page">
+        <div key={restraunt.id}>
+          <img className="restraunt-img" src={restraunt.imageUrl} />
+        </div>
+
+        <div className="single-restraunt-page-info">
+          <h3>{restraunt.name}</h3>
+          <p>{restraunt.description}</p>
+          <p>{restraunt.pricerange}</p>
+
+            <button onClick={this.handleAddToCart} type="submit">
+              Add To Cart
+            </button>
+        </div>
+      </div>
+    )
+}
+export default SingleRestaurant
