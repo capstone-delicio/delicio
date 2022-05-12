93dc5d6e06d33cb2ae00f45046b60729a9e4d1b9
Renee Du <doozyu@gmail.com>
2022-05-12 09:21:14 -0400

    userprofile styled

--- client/components/EditUser.js
+++ client/components/EditUser.js
@@ -3,13 +3,15 @@ import axios from 'axios'
 import { useHistory, useParams } from 'react-router-dom'
 import { updateUserThunk } from '../store/auth'
 import {useSelector,useDispatch} from 'react-redux'
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 
 const EditUser = () => {
     const { id } = useParams()
     let history = useHistory()
     const user = useSelector(state=>state.auth)
-    console.log('user',user)
     const dispatch = useDispatch()
 
     const [formState, setFormState] = useState({
@@ -45,51 +47,60 @@ const EditUser = () => {
         <div>
             <div className='container'>
                 <div className='profile'>
+                <Grid
+                      container
+                      alignItems="center"
+                      justifyContent="center"
+                      direction="column"
+                    >
                     <h2 className='profile-info'>Update User</h2>
                     <form onSubmit={e => SubmitHandler(e)}>
-                        <div className='form-group'>
-                            <input type='text' className='form-control form-control-lg'
-                                placeholder='Enter Your First Name'
+
+                        <Grid className='form-group'>
+                            <TextField type='text' className='form-control form-control-lg'
+                                label='Enter Your First Name'
                                 name='first_name'
                                 defaultValue={first_name}
-                                onChange={onChangeHandler}
+                                onChange={e => onChangeHandler(e)}
                             />
-                        </div>
-                        <div className='form-group'>
-                            <input type='text' className='form-control form-control-lg'
-                                placeholder='Enter Your Last Name'
+                       </Grid>
+                        <Grid className='form-group'>
+                            <TextField type='text' className='form-control form-control-lg'
+                                label='Enter Your Last Name'
                                 name='last_name'
                                 defaultValue={last_name}
                                 onChange={e => onChangeHandler(e)}
                             />
-                        </div>
-                        <div className='form-group'>
-                            <input type='text' className='form-control form-control-lg'
-                                placeholder='Enter Your Email'
+                        </Grid>
+                        <Grid className='form-group'>
+                            <TextField type='text' className='form-control form-control-lg'
+                                label='Enter Your Email'
                                 name='email'
                                 defaultValue={email}
                                 onChange={e => onChangeHandler(e)}
                             />
-                        </div>
-                        <div className='form-group'>
-                            <input type='text' className='form-control form-control-lg'
-                                placeholder='Enter Your Prederred City'
+                        </Grid>
+                        <Grid className='form-group'>
+                            <TextField type='text' className='form-control form-control-lg'
+                                label='Enter Your Prederred City'
                                 name='preferred_city'
                                 defaultValue={preferred_city}
                                 onChange={e => onChangeHandler(e)}
                             />
-                        </div>
-                        <div className='form-group'>
-                            <input type='text' className='form-control form-control-lg'
-                                placeholder='Enter Your Phone'
+                        </Grid>
+                        <Grid className='form-group'>
+                            <TextField type='text' className='form-control form-control-lg'
+                                label='Enter Your Phone'
                                 name='phone_number'
                                 defaultValue={phone_number}
                                 onChange={e => onChangeHandler(e)}
                             />
-                        </div>
+                        </Grid>
 
                         <button className='btn btn-warning btn-block' onChange = {e => SubmitHandler(e)}>Update User</button>
+
                       </form>
+                      </Grid>
                 </div>
 
             </div>
--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
         <div>
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
-          <Link to="/userprofile">Users Profile</Link>
+          <Link to="/edituser">Users Profile</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
