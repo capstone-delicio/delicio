bc6fd93b99fc2c2caadbda60a40de78d6c405746
yajessicachen <yajessicachen@gmail.com>
2022-05-19 16:32:19 -0400

    finished end swipe story

--- client/Routes.js
+++ client/Routes.js
@@ -1,29 +1,30 @@
-import React, { Component, Fragment } from "react";
-import { connect } from "react-redux";
-import { withRouter, Route, Switch, Redirect } from "react-router-dom";
-import Signup from "./components/SignUp";
-import Login from "./components/Login";
-import Questions from "./components/Questions";
-import Home from "./components/Home";
-import { me } from "./store";
-import EditUser from "./components/EditUser";
-import EventInput from "./components/EventInput";
-import SwipePage from "./components/SwipePage";
-import SearchUsers from "./components/SearchUsers";
-import SingleRestaurant from "./components/SingleRestaurant";
-import Timer from "./components/Timer";
-import Events from "./components/Events";
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom'
+import Signup from './components/SignUp'
+import Login from './components/Login'
+import Questions from './components/Questions'
+import Home from './components/Home'
+import { me } from './store'
+import EditUser from './components/EditUser'
+import EventInput from './components/EventInput'
+import SwipePage from './components/SwipePage'
+import SearchUsers from './components/SearchUsers'
+import SingleRestaurant from './components/SingleRestaurant'
+import Timer from './components/Timer'
+import Events from './components/Events'
+import EndSwipeStory from './components/EndSwipeStory'
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
   componentDidMount() {
-    this.props.loadInitialData();
+    this.props.loadInitialData()
   }
 
   render() {
-    const { isLoggedIn } = this.props;
+    const { isLoggedIn } = this.props
 
     return (
       <div>
@@ -31,17 +32,13 @@ class Routes extends Component {
           <Switch>
             <Route path="/home" component={Home} />
             <Route path="/questions" component={Questions} />
-
+            <Route path="/endswipestory" component={EndSwipeStory} />
             <Route path="/edituser" component={EditUser} />
             <Route path="/timer" component={Timer} />
-
             <Route path="/card" component={SwipePage} />
             <Route path="/eventinput" component={EventInput} />
-
             <Route path="/search-friends" component={SearchUsers} />
-
             <Route path="/singlerestaurant" component={SingleRestaurant} />
-
             <Route path="/events" component={Events} />
 
             <Redirect to="/home" />
@@ -54,7 +51,7 @@ class Routes extends Component {
           </Switch>
         )}
       </div>
-    );
+    )
   }
 }
 
@@ -66,17 +63,17 @@ const mapState = (state) => {
     // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
     // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
     isLoggedIn: !!state.auth.id,
-  };
-};
+  }
+}
 
 const mapDispatch = (dispatch) => {
   return {
     loadInitialData() {
-      dispatch(me());
+      dispatch(me())
     },
-  };
-};
+  }
+}
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes));
+export default withRouter(connect(mapState, mapDispatch)(Routes))
