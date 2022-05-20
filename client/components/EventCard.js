41c4558c5db28de24eec97c091e6dc084d29700d
Vida Feng <vidaffeng@gmail.com>
2022-05-20 12:23:43 -0400

    getsingleEvent thunk works

--- client/components/EventCard.js
+++ client/components/EventCard.js
@@ -1,20 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { getEventThunk } from "../store/event";
 
-const EventCard = () => {
+const EventCard = (props) => {
   //   const eventPicksStore = useSelector((state) => state.eventPicks);
+
+  const eventStore = useSelector((state) => state.event);
+  const dispatch = useDispatch();
+
   //   const user = useSelector((state) => {
   //     return state.auth;
   //   });
 
   //   clickable component
 
-  // eventname
-  // eventdate
-  // eventtime
-  // status: SOME MESSAGE HERE
+  useEffect(() => {
+    dispatch(getEventThunk(props.eventId));
+    // console.log("inside UseEffect", eventStore.singleEvent);
+    // grab eventId
+    // eventname
+    // eventdate
+    // eventtime
+    // status SOME MSG HERE
+  }, []);
 
-  return <h1>EventCard PAGE</h1>;
+  return <h1>{props.eventId}</h1>;
 };
 
 export default EventCard;
