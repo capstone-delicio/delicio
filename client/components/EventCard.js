e65aad557f2bba70514a5efdbe055ac33fe4685f
Rachel Or <rachelor7@gmail.com>
2022-05-21 10:27:09 -0500

    fixed dispatch _getDbRestPhotos

--- client/components/EventCard.js
+++ client/components/EventCard.js
@@ -168,7 +168,7 @@ const EventCard = (props) => {
 
   function handleClickVote() {
     dispatch(_getDbRestPhotos(event.id, user.id));
-    history.push('/card');
+    // history.push('/card');
   }
 
   // card
